4237bfd1a08b8dda314f5c02d450b60f7ac1daf7
Uriahas <chookua@gmail.com>
2018-09-06 15:41:28 +0300

    Add project files.

---
+++ DxSample/MainForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace DxSample {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.OrdersGrid = new DevExpress.XtraGrid.GridControl();
+            this.ordersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.NorthwindDataSet = new System.Data.DataSet();
+            this.dtOrders = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.dataColumn4 = new System.Data.DataColumn();
+            this.OrdersGridView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colOrderID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colOrderDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colFreight = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colShipName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar1 = new DevExpress.XtraBars.Bar();
+            this.PageSizeEditor = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemComboBox1 = new DevExpress.XtraEditors.Repository.RepositoryItemComboBox();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.repositoryItemTextEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            this.repositoryItemButtonEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemComboBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemButtonEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OrdersGrid
+            // 
+            this.OrdersGrid.DataSource = this.ordersBindingSource;
+            this.OrdersGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OrdersGrid.EmbeddedNavigator.Buttons.First.Visible = false;
+            this.OrdersGrid.EmbeddedNavigator.Buttons.Last.Visible = false;
+            this.OrdersGrid.EmbeddedNavigator.Buttons.NextPage.Visible = false;
+            this.OrdersGrid.EmbeddedNavigator.Buttons.PrevPage.Visible = false;
+            this.OrdersGrid.EmbeddedNavigator.CustomButtons.AddRange(new DevExpress.XtraEditors.NavigatorCustomButton[] {
+            new DevExpress.XtraEditors.NavigatorCustomButton(0, 1, false, true, "", "PrevPage"),
+            new DevExpress.XtraEditors.NavigatorCustomButton(1, 4, true, true, "", "NextPage")});
+            this.OrdersGrid.EmbeddedNavigator.ButtonClick += new DevExpress.XtraEditors.NavigatorButtonClickEventHandler(this.EmbeddedNavigator_ButtonClick);
+            this.OrdersGrid.Location = new System.Drawing.Point(0, 29);
+            this.OrdersGrid.MainView = this.OrdersGridView;
+            this.OrdersGrid.Name = "OrdersGrid";
+            this.OrdersGrid.Size = new System.Drawing.Size(429, 232);
+            this.OrdersGrid.TabIndex = 0;
+            this.OrdersGrid.UseEmbeddedNavigator = true;
+            this.OrdersGrid.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.OrdersGridView});
+            // 
+            // ordersBindingSource
+            // 
+            this.ordersBindingSource.DataMember = "Orders";
+            this.ordersBindingSource.DataSource = this.NorthwindDataSet;
+            // 
+            // NorthwindDataSet
+            // 
+            this.NorthwindDataSet.DataSetName = "Norhtwind";
+            this.NorthwindDataSet.Tables.AddRange(new System.Data.DataTable[] {
+            this.dtOrders});
+            // 
+            // dtOrders
+            // 
+            this.dtOrders.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3,
+            this.dataColumn4});
+            this.dtOrders.TableName = "Orders";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "OrderID";
+            this.dataColumn1.DataType = typeof(int);
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "OrderDate";
+            this.dataColumn2.DataType = typeof(System.DateTime);
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.ColumnName = "Freight";
+            this.dataColumn3.DataType = typeof(decimal);
+            // 
+            // dataColumn4
+            // 
+            this.dataColumn4.ColumnName = "ShipName";
+            // 
+            // OrdersGridView
+            // 
+            this.OrdersGridView.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colOrderID,
+            this.colOrderDate,
+            this.colFreight,
+            this.colShipName});
+            this.OrdersGridView.GridControl = this.OrdersGrid;
+            this.OrdersGridView.Name = "OrdersGridView";
+            // 
+            // colOrderID
+            // 
+            this.colOrderID.FieldName = "OrderID";
+            this.colOrderID.Name = "colOrderID";
+            this.colOrderID.Visible = true;
+            this.colOrderID.VisibleIndex = 0;
+            // 
+            // colOrderDate
+            // 
+            this.colOrderDate.FieldName = "OrderDate";
+            this.colOrderDate.Name = "colOrderDate";
+            this.colOrderDate.Visible = true;
+            this.colOrderDate.VisibleIndex = 1;
+            // 
+            // colFreight
+            // 
+            this.colFreight.FieldName = "Freight";
+            this.colFreight.Name = "colFreight";
+            this.colFreight.Visible = true;
+            this.colFreight.VisibleIndex = 2;
+            // 
+            // colShipName
+            // 
+            this.colShipName.FieldName = "ShipName";
+            this.colShipName.Name = "colShipName";
+            this.colShipName.Visible = true;
+            this.colShipName.VisibleIndex = 3;
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.PageSizeEditor});
+            this.barManager1.MaxItemId = 3;
+            this.barManager1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemComboBox1,
+            this.repositoryItemTextEdit1,
+            this.repositoryItemButtonEdit1});
+            // 
+            // bar1
+            // 
+            this.bar1.BarName = "Tools";
+            this.bar1.DockCol = 0;
+            this.bar1.DockRow = 0;
+            this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.PageSizeEditor)});
+            this.bar1.Text = "Tools";
+            // 
+            // PageSizeEditor
+            // 
+            this.PageSizeEditor.Caption = "Page Size";
+            this.PageSizeEditor.Edit = this.repositoryItemComboBox1;
+            this.PageSizeEditor.EditValue = "10";
+            this.PageSizeEditor.Id = 0;
+            this.PageSizeEditor.Name = "PageSizeEditor";
+            this.PageSizeEditor.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.Caption;
+            this.PageSizeEditor.EditValueChanged += new System.EventHandler(this.PageSizeEditor_EditValueChanged);
+            // 
+            // repositoryItemComboBox1
+            // 
+            this.repositoryItemComboBox1.AutoHeight = false;
+            this.repositoryItemComboBox1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemComboBox1.Items.AddRange(new object[] {
+            "10",
+            "50",
+            "100",
+            "200"});
+            this.repositoryItemComboBox1.Name = "repositoryItemComboBox1";
+            this.repositoryItemComboBox1.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor;
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(429, 29);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 261);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(429, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 29);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 232);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(429, 29);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 232);
+            // 
+            // repositoryItemTextEdit1
+            // 
+            this.repositoryItemTextEdit1.AutoHeight = false;
+            this.repositoryItemTextEdit1.Name = "repositoryItemTextEdit1";
+            // 
+            // repositoryItemButtonEdit1
+            // 
+            this.repositoryItemButtonEdit1.AutoHeight = false;
+            this.repositoryItemButtonEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.repositoryItemButtonEdit1.Name = "repositoryItemButtonEdit1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 261);
+            this.Controls.Add(this.OrdersGrid);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Name = "MainForm";
+            this.Text = "DX Sample";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtOrders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemComboBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemButtonEdit1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl OrdersGrid;
+        private DevExpress.XtraGrid.Views.Grid.GridView OrdersGridView;
+        private System.Windows.Forms.BindingSource ordersBindingSource;
+        private DevExpress.XtraGrid.Columns.GridColumn colOrderID;
+        private DevExpress.XtraGrid.Columns.GridColumn colOrderDate;
+        private DevExpress.XtraGrid.Columns.GridColumn colFreight;
+        private DevExpress.XtraGrid.Columns.GridColumn colShipName;
+        private System.Data.DataSet NorthwindDataSet;
+        private System.Data.DataTable dtOrders;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Data.DataColumn dataColumn4;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar1;
+        private DevExpress.XtraBars.BarEditItem PageSizeEditor;
+        private DevExpress.XtraEditors.Repository.RepositoryItemComboBox repositoryItemComboBox1;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit repositoryItemTextEdit1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit repositoryItemButtonEdit1;
+    }
+}
+
---
+++ DxSample/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using DevExpress.XtraBars;
+using DevExpress.XtraEditors;
+
+namespace DxSample {
+    public partial class MainForm : XtraForm {
+        private SqlDataAdapter DataAdapter;
+
+        public MainForm() {
+            InitializeComponent();
+            OrdersGridView.OptionsCustomization.AllowFilter = false;
+            OrdersGridView.OptionsCustomization.AllowGroup = false;
+            OrdersGridView.OptionsCustomization.AllowSort = false;
+            OrdersGridView.OptionsFind.AllowFindPanel = false;
+            OrdersGridView.OptionsView.ShowGroupPanel = false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            string selectCommandText = @"
+select OrderID, OrderDate, Freight, ShipName
+from Orders
+order by OrderID
+offset @offset rows
+fetch next @size rows only
+";
+            this.DataAdapter = new SqlDataAdapter(selectCommandText, connectionString);
+            this.DataAdapter.SelectCommand.Parameters.Add("@offset", SqlDbType.Int);
+            this.DataAdapter.SelectCommand.Parameters.Add("@size", SqlDbType.Int);
+            this.DataAdapter.SelectCommand.Parameters["@offset"].Value = 0;
+            this.DataAdapter.SelectCommand.Parameters["@size"].Value = PageSize;
+
+            this.DataAdapter.Fill(this.dtOrders);
+
+        }
+
+        private int fPageSize = 10;
+        public int PageSize {
+            get { return fPageSize; }
+            set { fPageSize = value; }
+        }
+
+        private void EmbeddedNavigator_ButtonClick(object sender, NavigatorButtonClickEventArgs e) {
+            if (e.Button.ButtonType != NavigatorButtonType.Custom) return;
+            e.Handled = true;
+            int offset = (int)DataAdapter.SelectCommand.Parameters["@offset"].Value;
+            string tag = (string)e.Button.Tag;
+            switch(tag) {
+                case "NextPage":
+                    offset += PageSize;
+                    break;
+                case "PrevPage":
+                    offset -= PageSize;
+                    break;
+            }
+            offset = Math.Max(0, offset);
+            OrdersGrid.EmbeddedNavigator.Buttons.CustomButtons[0].Enabled = offset > 0;
+            DataAdapter.SelectCommand.Parameters["@offset"].Value = offset;
+            dtOrders.Clear();
+            DataAdapter.Fill(dtOrders);
+            OrdersGrid.EmbeddedNavigator.Buttons.CustomButtons[1].Enabled = dtOrders.Rows.Count == PageSize;
+        }
+
+        private void PageSizeEditor_EditValueChanged(object sender, EventArgs e) {
+            BarEditItem editor = (BarEditItem)sender;
+            PageSize = int.Parse((string)editor.EditValue);
+            DataAdapter.SelectCommand.Parameters["@size"].Value = PageSize;
+            dtOrders.Clear();
+            DataAdapter.Fill(dtOrders);
+        }
+    }
+}
